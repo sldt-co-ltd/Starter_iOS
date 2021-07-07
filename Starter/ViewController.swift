243b9d026c0b66d176fcacf56320ca12a479dabb
i <iasandcb@gmail.com>
2021-07-07 15:58:23 +0900

    Initial Commit

---
+++ Starter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Starter
+//
+//  Created by Alex Lee on 2021/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
